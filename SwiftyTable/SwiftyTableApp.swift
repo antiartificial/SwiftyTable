15c72133b14919bcc3f33f3c55b0df2c37d67225
Aaron Barton <antiartificial@gmail.com>
2024-07-10 19:05:39 -0700

    Initial Commit

---
+++ SwiftyTable/SwiftyTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyTableApp.swift
+//  SwiftyTable
+//
+//  Created by Aaron Barton on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
